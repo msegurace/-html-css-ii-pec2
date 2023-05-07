8efb3329ee06ecace74e2756ddb6d1923fb3d44a
Moisés Segura Cedrés <msegurace@uoc.edu>
2023-05-07 09:15:12 +0100

    Página 1

---
+++ PEC2/uoc-boilerplate/src/assets/scripts/main.js
@@ -0,0 +1,27 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+} )();
+
+const { readFileSync } = require('fs')
+
+const posthtml = require('posthtml')
+const include = require('posthtml-include')
+
+const html = readFileSync('index.html')
+
+posthtml([ include({ encoding: 'utf8' }) ])
+  .process(html)
+  .then((result) => console.log(result.html));
